81950209ead115b1660cf638b5d9d68914997f40
sureshkumarbe <sureshkumarbecbe@gmail.com>
2025-08-01 08:40:48 +0530

    Variable Commit

--- app/src/main/java/com/airei/kotlinbasics/HelloKotlin.kt
+++ app/src/main/java/com/airei/kotlinbasics/HelloKotlin.kt
@@ -2,6 +2,29 @@ package com.airei.kotlinbasics
 
 fun main(){
     // Singleline Comment
+
+
+    /* A Variable is a named storage location in which you can store and manipulate data*/
+
+
+    /*Steps to create Variable
+        - Variable Declaration
+        - Type Inference
+        - Assigning Values
+
+        var : Keyword used to declare a mutable(changeable) variable
+        val : Keyword used to declare an immutable(constant) variable
+     */
+
+    // Mutable Variable
+    var myAge : Int = 25
+
+    // We can remove the type Int, because kotlin will automatically set the type based on the input value
+    var newAge = 25
+
+    // Immuatable Variable
+    val pi : Double = 3.14159
+
     println("Hello World")
 
     /*
